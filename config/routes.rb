79e4eff4c725f33ed53a0ad405020ecd9da563a8
JunHatanaka <nextseason6@gmail.com>
2022-03-13 00:33:43 +0900

    Add: rails new

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :users
+  # get '/users', to: 'users#index'
+  # get '/users/new', to: 'users#new'
+  # post '/users', to: 'users#create'
+  # get '/users/:id', to: 'users#show'
+  # get '/users/:id/edit', to: 'users#edit'
+  # patch '/users/id', to: 'users#update'
+  # delete '/users/:id', to: 'users#destroy'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
